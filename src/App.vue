09804f9ce3a5439ab877f409e7ae68d8c7b8dc4f
HHZKYO <1692189552@qq.com>
2024-11-20 14:38:06 +0800

    完成一级路由配置

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="app">
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-    <div class="box"></div>
     <router-view/>
   </div>
 </template>
 
-<style lang="less">
-.box{
-  width: 200px;
-  height: 200px;
-  background-color: pink;
+<script>
+export default {
+  data () {
+    return {}
+  }
 }
+</script>
+
+<style lang="less">
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,18 @@
+import Detail from '@/views/Detail.vue'
+import Layout from '@/views/Layout.vue'
+import Login from '@/views/Login.vue'
+import Register from '@/views/Register.vue'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  { path: '/login', component: Login },
+  { path: '/register', component: Register },
+  { path: '/layout', component: Layout },
+  { path: '/article/:id', component: Detail }
+]
 
 const router = new VueRouter({
   routes
