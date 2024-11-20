5af68912ed7634e2b227b752501d4b6fd8df1a17
HHZKYO <1692189552@qq.com>
2024-11-20 15:34:55 +0800

    初步完成登录静态页面

--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,6 +1,9 @@
-import { Button, Tabbar, TabbarItem } from 'vant'
+import { NavBar, Form, Field, Button, Tabbar, TabbarItem } from 'vant'
 import Vue from 'vue'
 
 Vue.use(Button)
 Vue.use(Tabbar)
 Vue.use(TabbarItem)
+Vue.use(Form)
+Vue.use(Field)
+Vue.use(NavBar)
