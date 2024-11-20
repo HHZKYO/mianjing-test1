53f0ff106ed6496e47063ad9b0d668ddc54376a2
HHZKYO <1692189552@qq.com>
2024-11-20 15:58:43 +0800

    完成登录页校验规则设置

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,18 +7,24 @@
     <van-form @submit="onSubmit">
       <van-field
         v-model="username"
-        name="用户名"
+        name="username"
         label="用户名"
-        placeholder="用户名"
-        :rules="[{ required: true, message: '请填写用户名' }]"
+        placeholder="请输入用户名"
+        :rules="[
+          { required: true, message: '请填写用户名' },
+          { pattern: /^\w{5,8}$/, message: '用户名为5-8个字符'}
+        ]"
       />
       <van-field
         v-model="password"
         type="password"
-        name="密码"
+        name="password"
         label="密码"
-        placeholder="密码"
-        :rules="[{ required: true, message: '请填写密码' }]"
+        placeholder="请输入密码"
+        :rules="[
+          { required: true, message: '请填写密码' },
+          { pattern: /^\w{6}$/, message: '密码为6个字符'}
+        ]"
       />
       <div style="margin: 16px;">
         <van-button block type="info" native-type="submit">提交</van-button>
