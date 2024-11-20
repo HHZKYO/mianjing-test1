ffc4f2b7252e10acf008b9cc16ea670f1e715fdb
HHZKYO <1692189552@qq.com>
2024-11-20 15:15:23 +0800

    配置二级路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,11 @@
+import Article from '@/views/Article.vue'
+import Collect from '@/views/Collect.vue'
 import Detail from '@/views/Detail.vue'
 import Layout from '@/views/Layout.vue'
+import Like from '@/views/Like.vue'
 import Login from '@/views/Login.vue'
 import Register from '@/views/Register.vue'
+import User from '@/views/User.vue'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
@@ -10,8 +14,18 @@ Vue.use(VueRouter)
 const routes = [
   { path: '/login', component: Login },
   { path: '/register', component: Register },
-  { path: '/layout', component: Layout },
-  { path: '/article/:id', component: Detail }
+  { path: '/article/:id', component: Detail },
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/article',
+    children: [
+      { path: '/article', component: Article },
+      { path: '/like', component: Like },
+      { path: '/collect', component: Collect },
+      { path: '/user', component: User }
+    ]
+  }
 ]
 
 const router = new VueRouter({
