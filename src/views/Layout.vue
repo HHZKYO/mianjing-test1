3865f9e5f71c5e81ebdb991cd069746bf6238190
HHZKYO <1692189552@qq.com>
2024-11-20 14:49:30 +0800

    实现底部导航栏图标

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -2,7 +2,12 @@
   <div class="layout-page">
     <div>首页架子 - 内容区域</div>
 
-    <div>底部tabbar - 区域</div>
+    <van-tabbar>
+      <van-tabbar-item icon="coupon-o">文章</van-tabbar-item>
+      <van-tabbar-item icon="star-o">收藏</van-tabbar-item>
+      <van-tabbar-item icon="like-o">喜欢</van-tabbar-item>
+      <van-tabbar-item icon="manager-o">我的</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
