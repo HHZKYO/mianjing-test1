7a458e35016ea749f84d5fe89a0b6889ed578764
HHZKYO <1692189552@qq.com>
2024-11-21 16:17:13 +0800

    在请求拦截器中统一携带token

--- src/api/article.js
+++ src/api/article.js
@@ -1,18 +1,12 @@
 import request from '@/utils/request'
-import { getToken } from '@/utils/storage'
 
 export const getArticles = (obj) => {
-  const token = getToken()
   return request.get('/interview/query', {
     // 配置get请求参数
     params: {
       current: obj.current || 1,
       pageSize: obj.pageSize || 10,
       sorter: obj.sorter
-    },
-    // headers请求头，配置token
-    headers: {
-      Authorization: `Bearer ${token}`
     }
   })
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { Toast } from 'vant'
+import { getToken } from './storage'
 
 const instance = axios.create({
   baseURL: 'http://interview-api-t.itheima.net/h5/',
@@ -8,17 +9,17 @@ const instance = axios.create({
 
 // 添加请求拦截器
 instance.interceptors.request.use(function (config) {
-  // 在发送请求之前做些什么
+  const token = getToken()
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
   return config
 }, function (error) {
-  // 对请求错误做些什么
   return Promise.reject(error)
 })
 
 // 添加响应拦截器
 instance.interceptors.response.use(function (response) {
-  // 2xx 范围内的状态码都会触发该函数。
-  // 对响应数据做点什么
   return response
 }, function (error) {
   // 有错误响应，后台正常返回了错误信息
